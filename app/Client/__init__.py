5abc8ebaa4cd25ca0d1c9991f2943b62a48063ab
kohmet <k.j.hirner.wisdom@gmail.com>
2020-06-28 12:50:15 -0700

    added install script install.sh and MyMoneyMaker.desktop

--- app/Client/__init__.py
+++ app/Client/__init__.py
@@ -1,12 +1,14 @@
 from PyQt5.QtCore import QObject,QRunnable,QObject,QThread,QThreadPool
 from PyQt5.QtWidgets import QApplication,QMainWindow
 from PyQt5 import uic
+from PyQt5.QtGui import QIcon,QPixmap,QImage
 from .Loggin.Login import Login
 from .MenuBar.MenuBar import MenuBar
 from .Review.Review import Review
 from .NewEntry.NewEntry import NewEntry
 from .Charting.Charting import Charting
 import os,sys,json
+from pathlib import Path
 
 class Main(QMainWindow):
     def __init__(self):
@@ -28,7 +30,7 @@ class Main(QMainWindow):
         self.stacks['newEntry']=NewEntry(self)
         self.stacks['charting']=Charting(self)
         self.menubar=MenuBar(self)
-
+        self.setWindowIcon(QIcon(QPixmap(str(Path("Client/MainWindow/program.png")))))
         print(self.user,"user"*10)
         self.show()
 
